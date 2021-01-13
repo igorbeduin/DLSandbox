9bf7f9bc83c427e0ef59733ba3aba3567a2249ed
Igor Beduin <igor.rob.96@gmail.com>
2021-01-13 19:47:37 -0300

    First model of SpralNet (only denses)

--- net.py
+++ net.py
@@ -1,5 +1,26 @@
 """ Module responsible for aggregate the distinctive net configurations used for training."""
 
+import tensorflow
+from tensorflow import keras
+from tensorflow.keras import layers
+
 class SpiralNetV1:
-    def __init__(self)
-    pass
+    def __init__(self, input=2, outputs=2):
+        self._model = keras.Sequential(name="SpiralNetV1")
+        self._model.add(keras.Input(shape=(input,)))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(16, activation="relu"))
+        self._model.add(layers.Dense(outputs, activation="softmax", name="output"))
+    
+    @property
+    def model(self):
+        return self._model
